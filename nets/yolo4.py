cf6004bec6f019b099e265cf4252756165f24331
Rory-Godwin <rgry.godwin@gmail.com>
2021-03-31 14:46:51 +0100

    Mod 3
    Lines commented out 145                                             Lines commented in 100,102,104,107,113,114,146  fixed the previous mistake by adding some P3 back as a reduced function

--- nets/yolo4.py
+++ nets/yolo4.py
@@ -97,21 +97,21 @@ def yolo_body(inputs, num_anchors, num_classes):
     P4 = make_five_convs(P4,256)
 
     # 26,26,256 -> 26,26,128 -> 52,52,128
-    #P4_upsample = compose(DarknetConv2D_BN_Leaky(128, (1,1)), UpSampling2D(2))(P4)
+    P4_upsample = compose(DarknetConv2D_BN_Leaky(128, (1,1)), UpSampling2D(2))(P4)
     # 52,52,256 -> 52,52,128
-    #P3 = DarknetConv2D_BN_Leaky(128, (1,1))(feat1)
+    P3 = DarknetConv2D_BN_Leaky(128, (1,1))(feat1)
     # 52,52,128 + 52,52,128 -> 52,52,256
-    #P3 = Concatenate()([P3, P4_upsample])
+    P3 = Concatenate()([P3, P4_upsample])
 
     # 52,52,256 -> 52,52,128 -> 52,52,256 -> 52,52,128 -> 52,52,256 -> 52,52,128
-    #P3 = make_five_convs(P3,128)
+    P3 = make_five_convs(P3,128)
     
     #---------------------------------------------------#
     #   第三个特征层
     #   y3=(batch_size,52,52,3,85)
     #---------------------------------------------------#
-    #P3_output = DarknetConv2D_BN_Leaky(256, (3,3))(P3)
-    #P3_output = DarknetConv2D(num_anchors*(num_classes+5), (1,1), kernel_initializer=keras.initializers.normal(mean=0.0, stddev=0.01))(P3_output)
+    P3_output = DarknetConv2D_BN_Leaky(256, (3,3))(P3)
+    P3_output = DarknetConv2D(num_anchors*(num_classes+5), (1,1), kernel_initializer=keras.initializers.normal(mean=0.0, stddev=0.01))(P3_output)
 
     # 52,52,128 -> 26,26,256
     #P3_downsample = ZeroPadding2D(((1,0),(1,0)))(P3)
@@ -142,8 +142,8 @@ def yolo_body(inputs, num_anchors, num_classes):
     #---------------------------------------------------#
     P5_output = DarknetConv2D_BN_Leaky(1024, (3,3))(P5)
     P5_output = DarknetConv2D(num_anchors*(num_classes+5), (1,1), kernel_initializer=keras.initializers.normal(mean=0.0, stddev=0.01))(P5_output)
-    return Model(inputs, [P5_output, P4_output])
-   #return Model(inputs, [P5_output, P4_output, P3_output])
+    #return Model(inputs, [P5_output, P4_output])
+    return Model(inputs, [P5_output, P4_output, P3_output])
    
 
 #---------------------------------------------------#
